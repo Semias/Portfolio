52f3084113f4a95ef4c057bad57d97e3f7a7773b
Stefan <62961421+Semias@users.noreply.github.com>
2026-01-06 00:01:32 +0100

    feat: add header and first content

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss'],
 })
 export class App {
-  protected readonly title = signal('portfolio-angular');
+  protected readonly title = signal('Stefan Gall');
 }
